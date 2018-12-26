97e658b800c47538cdb172ea96627e04b0ec5a93
Rainbow Spike <Dr-Yukon@users.noreply.github.com>
2018-12-26 23:34:37 +1000

    Create lang.php
    For toolbar

---
+++ lang/fr/lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['note_picker']  = 'Note';
+
+$lang['tb_note']      = 'Note';
+$lang['tb_tip']       = 'Conseil';
+$lang['tb_important'] = 'Important';
+$lang['tb_warning']   = 'Alerte';
