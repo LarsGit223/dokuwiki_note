732f8c47fabfa00c94fac85a346632b9ae33ee1e
Rainbow Spike <Dr-Yukon@users.noreply.github.com>
2018-12-26 23:35:25 +1000

    Create lang.php
    For toolbar

---
+++ lang/ru/lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['note_picker']  = 'Заметки';
+
+$lang['tb_note']      = 'Заметка';
+$lang['tb_tip']       = 'Совет';
+$lang['tb_important'] = 'Важно';
+$lang['tb_warning']   = 'Внимание';
